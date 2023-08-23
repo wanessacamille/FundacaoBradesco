6504e033a2e80b5bd3a59f78c5a8d586cef0217a
Wanessa Camille Costa da Silva <142358738+wanessacamille@users.noreply.github.com>
2023-08-23 19:47:03 -0300

    Add files via upload

---
+++ FIC/Linguagens de Programação/nota.py
@@ -0,0 +1,12 @@
+V = []
+i = 0
+while (i <20):
+    V.append(float(input('digite a nota: ')))
+    i=i +1
+
+i = 0
+print("nota > 7.5: ")
+while (i<20):
+   if V [i] > 7.5:
+      print(V[i])
+   i = i + 1
